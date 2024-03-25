2a98a6af8dad33e17901cd98231f507beefffe7a
Thomas Grenet <thomas.grenet@iut-dijon.u-bourgogne.fr>
2024-03-25 14:08:52 +0100

    Initialize commit

---
+++ TPBlackJack/src/main/java/com/mycompany/tpblackjack/TPBlackJack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tpblackjack;
+
+/**
+ *
+ * @author TotoZone
+ */
+public class TPBlackJack {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
