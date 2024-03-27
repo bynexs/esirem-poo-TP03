5241190bfae3d5853284e4b41a45902634e5cd5b
Thomas Grenet <thomas.grenet@iut-dijon.u-bourgogne.fr>
2024-03-27 12:50:26 +0100

    Ajout de la méthode pick et ajout de la logique du jeu dans le main

--- TPBlackJack/src/main/java/com/mycompany/tpblackjack/TPBlackJack.java
+++ TPBlackJack/src/main/java/com/mycompany/tpblackjack/TPBlackJack.java
@@ -16,7 +16,8 @@ public class TPBlackJack {
         List<Card> cards = BlackjackManager.suffle(BlackjackManager.CreationDeck());
         Player player = new Player();
         Player dealer = new Player();
-
+        boolean sortie = false;
+        
         BlackjackManager.InitialisationGame(cards, player, dealer);
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
@@ -25,5 +26,28 @@ public class TPBlackJack {
 
         dealer.setName("Robot");
         player.setBalance(1000);
-    }
+        
+        System.out.println("Combien veux-tu parier ?");
+        int balance = myObj.nextInt();
+        if (balance > 0 && balance <= player.getBalance()) {
+            player.setBalance(player.getBalance() - balance);
+        }
+        while (sortie != true || player.getHand().getValue() <= 21) {
+            BlackjackManager.DisplayPlayerHandAndDealearCard(player, dealer);
+            System.out.println("Veux-tu repiocer ?");
+            String value = myObj.nextLine();
+            if(value.equals("oui") || value.equals("o") || value.equals("yes") || value.equals("y")){
+                player = BlackjackManager.PickCard(cards, player);
+            }else{
+                sortie = true;
+            }
+        }
+        
+        while(dealer.getHand().getValue() <= 16){
+            System.out.println("La banque repioche !");
+            dealer = BlackjackManager.PickCard(cards, dealer);
+        }
+        
+        BlackjackManager.ShowAllCard(player, dealer);
+    }   
 }
