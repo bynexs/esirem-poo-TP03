8f0187987c76390b249d25b191f4cc0471460fa3
Thomas Grenet <thomas.grenet@iut-dijon.u-bourgogne.fr>
2024-03-27 11:57:30 +0100

    Initialisation du main

--- TPBlackJack/src/main/java/com/mycompany/tpblackjack/BlackjackManager.java
+++ TPBlackJack/src/main/java/com/mycompany/tpblackjack/BlackjackManager.java
@@ -4,7 +4,6 @@
  */
 package com.mycompany.tpblackjack;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -21,7 +20,7 @@ public class BlackjackManager {
             for (int j = 1; j < 14; j++) {
                 Card card = new Card();
                 card.setValue(j);                       //ajout la valeur de j a card
-                card.setCardType(EnumCardType.values()[i]);                                            
+                card.setCardType(EnumCardType.values()[i]);
                 cards.add(card);                          //renvoie une card a l'objet cards
             }
 
@@ -29,41 +28,40 @@ public class BlackjackManager {
         return cards;
     }
 
-    public static List<Card> suffle(List<Card> cards){
+    public static List<Card> suffle(List<Card> cards) {
         Collections.shuffle(cards);
         return cards;
     }
-    
+
     public static void DisplayPlayerHandAndDealearCard(Player player, Player dealer) {
-        System.out.println(player.getName() + "à une main");
+        System.out.println(player.getName() + " à une main \n");
+        System.out.println("Balance: " + player.getBalance());
         for (Card card : player.getHand().getCards()) {
             System.out.println(card.getCardType().toString() + " avec valeur " + card.getValue());
         }
-        System.out.println(dealer.getName() + "à  une carte visible:");
-        
-        if(!dealer.getHand().getCards().isEmpty()) {
+        System.out.println(dealer.getName() + " à  une carte visible:");
+
+        if (!dealer.getHand().getCards().isEmpty()) {
             System.out.println(dealer.getHand().getCards().get(0).getCardType().toString() + " avec valeur " + dealer.getHand().getCards().get(0).getValue());
-        }
-        else {
+        } else {
             System.out.println("Pas de carte");
         }
     }
 
-    public void InitialisationGame(List<Card> cards){
-        Player player = new Player();
-        Player dealer = new Player();
+    public static void InitialisationGame(List<Card> cards, Player player, Player dealer) {
         Hand handPlayer = new Hand();
         Hand handDealer = new Hand();
-    
-        for(int i = 0; i < 4; i++){
+
+        for (int i = 0; i < 4; i++) {
             Card card = cards.get(i);
-            if(i % 2 == 0){
+            if (i % 2 == 0) {
                 handPlayer.setCards(card);
-            }else{
+            } else {
                 handDealer.setCards(card);
             }
         }
         player.setHand(handPlayer);
         dealer.setHand(handDealer);
+        DisplayPlayerHandAndDealearCard(player, dealer);
     }
 }
--- TPBlackJack/src/main/java/com/mycompany/tpblackjack/TPBlackJack.java
+++ TPBlackJack/src/main/java/com/mycompany/tpblackjack/TPBlackJack.java
@@ -1,10 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.tpblackjack;
 
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -14,5 +14,16 @@ public class TPBlackJack {
 
     public static void main(String[] args) {
         List<Card> cards = BlackjackManager.suffle(BlackjackManager.CreationDeck());
+        Player player = new Player();
+        Player dealer = new Player();
+
+        BlackjackManager.InitialisationGame(cards, player, dealer);
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Entre ton nom");
+        player.setName(myObj.nextLine());
+
+        dealer.setName("Robot");
+        player.setBalance(1000);
     }
 }
