a1ee9c20d3ec842c4a547f4f40d6843473cb7dc6
Thomas Grenet <thomas.grenet@iut-dijon.u-bourgogne.fr>
2024-03-28 19:35:20 +0100

    Finialisation de l'IHM du jeu et correction des différents bugs

--- Blackjack/src/main/java/com/blackjack/App.java
+++ Blackjack/src/main/java/com/blackjack/App.java
@@ -2,19 +2,27 @@ package com.blackjack;
 
 import com.entity.BlackjackManager;
 import com.entity.Card;
+import com.entity.EnumSpecialCard;
 import com.entity.Player;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.util.Duration;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -24,14 +32,16 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     private Stage primaryStage;
-    private Scene scene1, scene2, scene3;
+    private Scene scene1, scene2, scene3, scene4;
 
     private TextField usernameField = new TextField();
     private TextField betField = new TextField();
     private Player player = new Player();
     private Player dealer = new Player();
     private List<Card> cards = new ArrayList<>();
-        
+    private VBox panelPlayer;
+    private VBox panelDealer;
+
     @Override
     public void start(Stage primaryStage) {
 
@@ -54,14 +64,14 @@ public class App extends Application {
         newGameButton.setOnAction(e -> showNewGamePage());
         gridPane1.add(newGameButton, 0, 0);
 
-        /*Button loadGameButton = new Button("Charger partie");
+        Button loadGameButton = new Button("Charger partie");
         loadGameButton.setOnAction(e -> showPage("Charger partie"));
         gridPane1.add(loadGameButton, 0, 1);
 
         Button settingsButton = new Button("Paramètres");
         settingsButton.setOnAction(e -> showPage("Paramètres"));
-        gridPane1.add(settingsButton, 0, 2);*/
-        scene1 = new Scene(gridPane1, 700, 500);
+        gridPane1.add(settingsButton, 0, 2);
+        scene1 = new Scene(gridPane1, 300, 300);
 
         // Page 2 - Form for username and bet (Nouvelle partie)
         GridPane gridPane2 = new GridPane();
@@ -85,7 +95,7 @@ public class App extends Application {
         validateButton.setOnAction(e -> showGame());
         gridPane2.add(validateButton, 3, 5);
 
-        scene2 = new Scene(gridPane2, 300, 200);
+        scene2 = new Scene(gridPane2, 450, 200);
 
         BlackjackManager.InitialisationGame(cards, player, dealer);
 
@@ -93,12 +103,16 @@ public class App extends Application {
         gridPane3.setVgap(3);
         gridPane3.setHgap(6);
 
-        VBox panelPlayer = new VBox();
+        panelPlayer = new VBox();
         for (Card card : player.getHand().getCards()) {
-            panelPlayer.getChildren().add(new Label(card.getCardType().toString() + " avec valeur " + card.getValue()));
+            if (card.getSpecialCard() != EnumSpecialCard.CHIFFRE) {
+                panelPlayer.getChildren().add(new Label(card.getSpecialCard().toString() + " de " + card.getCardType().toString() + " avec valeur " + card.getValue()));
+            } else {
+                panelPlayer.getChildren().add(new Label(card.getCardType().toString() + " avec valeur " + card.getValue()));
+            }
         }
 
-        VBox panelDealer = new VBox();
+        panelDealer = new VBox();
         if (!dealer.getHand().getCards().isEmpty()) {
             panelDealer.getChildren().add(new Label(dealer.getHand().getCards().get(0).getCardType().toString() + " avec valeur " + dealer.getHand().getCards().get(0).getValue()));
         }
@@ -113,9 +127,35 @@ public class App extends Application {
 
         Button pickButton = new Button("Repiocher");
         pickButton.setOnAction(e -> ShowPickCardPlayer());
-        gridPane3.add(pickButton, 2, 5);
+        gridPane3.add(pickButton, 0, 5);
+
+        Button terminateButton = new Button("Terminer");
+        terminateButton.setOnAction(e -> {
+            try {
+                DealerPickCard();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+        gridPane3.add(terminateButton, 2, 5);
+
+        ColumnConstraints columnConstraints = new ColumnConstraints();
+        columnConstraints.setPercentWidth(33.33); // 33.33% de la largeur de la scène pour chaque colonne
+        gridPane3.getColumnConstraints().addAll(columnConstraints, columnConstraints, columnConstraints);
+
+        RowConstraints rowConstraints = new RowConstraints();
+        rowConstraints.setPercentHeight(16.67); // 16.67% de la hauteur de la scène pour chaque ligne
+        gridPane3.getRowConstraints().addAll(rowConstraints, rowConstraints, rowConstraints, rowConstraints, rowConstraints, rowConstraints);
 
         scene3 = new Scene(gridPane3, 500, 300);
+        Label label = null;
+        if ((player.getHand().getValue() > dealer.getHand().getValue() && player.getHand().getValue() <= 21) || (dealer.getHand().getValue() > 21)) {
+            label = new Label("C'est Gagner");
+        } else {
+            label = new Label("C'est Perdu");
+        }
+
+        scene4 = new Scene(label, 500, 300);
 
         primaryStage.setScene(scene1);
         primaryStage.setTitle("Blackjack");
@@ -139,9 +179,73 @@ public class App extends Application {
     }
 
     private void ShowPickCardPlayer() {
-        BlackjackManager.PickCard(cards, player);
+        if (player.getHand().getValue() < 21) {
+            BlackjackManager.PickCard(cards, player);
+            if (player.getHand().getValue() >= 21) {
+                ShowAllCard(player, dealer);
+                Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(5), event -> {
+                    primaryStage.setScene(scene4);
+                }));
+                timeline.play();
+                primaryStage.setScene(scene4);
+            }
+            // Mise à jour du panneau du joueur
+            panelPlayer.getChildren().clear(); // Efface les anciennes cartes
+            for (Card card : player.getHand().getCards()) {
+                if (card.getSpecialCard() != EnumSpecialCard.CHIFFRE) {
+                    panelPlayer.getChildren().add(new Label(card.getSpecialCard().toString() + " de " + card.getCardType().toString() + " avec valeur " + card.getValue()));
+                } else {
+                    panelPlayer.getChildren().add(new Label(card.getCardType().toString() + " avec valeur " + card.getValue()));
+                }
+            }
+        } else {
+            ShowAllCard(player, dealer);
+            Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(5), event -> {
+                primaryStage.setScene(scene4);
+            }));
+            timeline.play();
+            primaryStage.setScene(scene4);
+        }
     }
-    
+
+    private void showPage(String title) {
+        Label titleLabel = (Label) scene2.getRoot().getChildrenUnmodifiable().get(0);
+        titleLabel.setText(title);
+        primaryStage.setScene(scene2);
+    }
+
+    private void DealerPickCard() throws InterruptedException {
+        while (dealer.getHand().getValue() <= 16) {
+            dealer = BlackjackManager.PickCard(cards, dealer);
+        }
+        ShowAllCard(player, dealer);
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(5), event -> {
+            primaryStage.setScene(scene4);
+        }));
+        timeline.play();
+    }
+
+    public void ShowAllCard(Player player, Player dealer) {
+        panelPlayer.getChildren().clear();
+        panelDealer.getChildren().clear();
+
+        for (Card card : player.getHand().getCards()) {
+            if (card.getSpecialCard() != EnumSpecialCard.CHIFFRE) {
+                panelPlayer.getChildren().add(new Label(card.getSpecialCard().toString() + " de " + card.getCardType().toString() + " avec valeur " + card.getValue()));
+            } else {
+                panelPlayer.getChildren().add(new Label(card.getCardType().toString() + " avec valeur " + card.getValue()));
+            }
+        }
+
+        for (Card card : dealer.getHand().getCards()) {
+            if (card.getSpecialCard() != EnumSpecialCard.CHIFFRE) {
+                panelDealer.getChildren().add(new Label(card.getSpecialCard().toString() + " de " + card.getCardType().toString() + " avec valeur " + card.getValue()));
+            } else {
+                panelDealer.getChildren().add(new Label(card.getCardType().toString() + " avec valeur " + card.getValue()));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         launch();
     }
--- Blackjack/src/main/java/com/entity/BlackjackManager.java
+++ Blackjack/src/main/java/com/entity/BlackjackManager.java
@@ -19,10 +19,10 @@ public class BlackjackManager {
         List<Card> cards = new ArrayList<>();               //creation de la liste 
         int id = 1;
         for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 13; j++) {
+            for (int j = 1; j <= 13; j++) {
                 Card card = new Card();                       
                 card.setCardType(EnumCardType.values()[i]);
-                if(j >= 10){
+                if(j > 10){
                     card.setValue(10);
                     card.setSpecialCard(EnumSpecialCard.values()[id]);
                     id ++;
@@ -65,25 +65,5 @@ public class BlackjackManager {
         player.getHand().setCards(cards.get(0));
         cards.remove(temp);
         return player;
-    }
-
-    public static void ShowAllCard(Player player, Player dealer) {
-        System.out.println("La main de:" + player.getName());
-        for (Card card : player.getHand().getCards()) {
-            System.out.println(card.getCardType().toString() + " avec valeur " + card.getValue());
-        }
-        System.out.println("Total points: " + player.getHand().getValue());
-
-        System.out.println("La main de la Banque");
-        for (Card card : dealer.getHand().getCards()) {
-            System.out.println(card.getCardType().toString() + " avec valeur " + card.getValue());
-        }
-        System.out.println("Total points: " + dealer.getHand().getValue());
-
-        if(player.getHand().getValue() > dealer.getHand().getValue() && player.getHand().getValue() <= 21){
-            System.out.println("Joueur: " + player.getName() + " a gagné");
-        }else{
-            System.out.println("La banque a gagné");
-        }
     }
 }
