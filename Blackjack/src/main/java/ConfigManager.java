a62ea8720e2992b805f26ac5658e6103ca27d81d
Gwendal de Cecco <gwendal.dececco@gmail.com>
2024-03-28 16:56:14 +0100

    Gestion des paramètres. Connexion a la base de données. Vérification de l'Id. Modification des erreurs trouvés.

--- Blackjack/src/main/java/ConfigManager.java
+++ Blackjack/src/main/java/ConfigManager.java
@@ -1,17 +1,8 @@
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author user
@@ -23,88 +14,73 @@ import java.util.Properties;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import com.entity.Player;
 
 public class ConfigManager {
+
     private Properties config;
     private final String configFilePath = "param.txt";
-    
+
     public ConfigManager() {
         config = new Properties();
         loadConfig();
     }
-    
+
 // Lit le fichier param.txt en utilisant un FileInputStream, les paramètres sont chargés dans l'objet Properties
 // try mot clés en Java pour démarrer un bloc de code qui pourrait poser problème.   
     private void loadConfig() {
         try (FileInputStream fis = new FileInputStream(configFilePath)) {
             config.load(fis);
-            }
-        
-// catch mot clés pour gérer les erreurs de leccture 
-        catch(IOException e) {
+        } // catch mot clés pour gérer les erreurs de leccture 
+        catch (IOException e) {
             System.out.println("Erreurs lors de la lecture du fichier");
-			
-		}
+
         }
-    
- // Garde en mémoire la valeur minBal   
-    public void setMinBal(int minBal){
+    }
+
+    // Garde en mémoire la valeur minBal   
+    public void setMinBal(int minBal) {
         config.setProperty("minBal", String.valueOf(minBal));
         saveConfig();
     }
-    
- // Garde en mémoire la valeur maxBal
+
+    // Garde en mémoire la valeur maxBal
     public void setMaxBal(int maxBal) {
         config.setProperty("maxBal", String.valueOf(maxBal));
         saveConfig();
     }
-    
+
 // Cette méthode écrit les paramètres de configuration actuels dans le fichier param.txt en utilisant un FileOutputStream 
     private void saveConfig() {
-        try (FileOutputStream fos = new FileOutputStream(configFilePath)){
-                config.store(fos,"param.txt");
-        }
-        
-        catch(IOException e) {
+        try (FileOutputStream fos = new FileOutputStream(configFilePath)) {
+            config.store(fos, "param.txt");
+        } catch (IOException e) {
             System.out.println("Erreur lors de la sauvegarde du fichier");
-            
+
         }
-        
-        }
-    
- // Méthode pour vérifier l'Id du joueur et l'Id du fichier    
+
+    }
+
+    // Méthode pour vérifier l'Id du joueur et l'Id du fichier    
     public boolean isAuthorized(String userId) {
         String authorizedId = config.getProperty("authorizedId");
-        return authorizedId != null && authorizedId.equals(userId);       
+        return authorizedId != null && authorizedId.equals(userId);
     }
-    
+
 // Connexion à la base de donnée si l'Id est juste     
-    public Connection connectedtoBDD() {
-        if (isAuthorized(Player.getName()) ) {
-            try { 
-                String bddURL=config.getProperty("bddURL");
-                DriverManager.getConnection(bddURL);
-            }
-            catch(SQLException e) {
+    public Connection connectedtoBDD(Player player) {
+        Connection url = null;
+        if (isAuthorized(player.getName())) {
+            try {
+                String bddURL = config.getProperty("bddURL");
+                 url = DriverManager.getConnection(bddURL);
+            } catch (SQLException e) {
                 System.out.println("Impossible de se connecter a la base de données");
             }
-        }
-        else{
-                System.out.println("Identifiant incorrect. Accés refusé");
-                    
-            }
-            }
-        
-        
-        }
-    
-    
-    
-    
-  
-    
-    
-            
-    
+        } else {
+            System.out.println("Identifiant incorrect. Accés refusé");
 
+        }
+        return url;
+    }
+}
